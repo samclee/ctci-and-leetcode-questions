826e6764505807893b8fb266a537176a5da9ab8a
samclee <samclee@gmail.com>
2018-11-25 23:16:09 -0800

    p. sure this works

--- basic-stuff/merge-sort/main.cpp
+++ basic-stuff/merge-sort/main.cpp
@@ -11,17 +11,47 @@ void printVec(const vector<int>& ary) {
 
 void merge(vector<int> &v, int l, int r) {
 	int m = (l + r) / 2;
+	int leftPtr = l;
+	int rightPtr = m + 1;
+	vector<int> helper;
+
+	while (leftPtr <= m && rightPtr <= r) {
+		if (v[leftPtr] <= v[rightPtr]) {
+			helper.push_back(v[leftPtr]);
+			leftPtr++;
+		}
+		else {
+			helper.push_back(v[rightPtr]);
+			rightPtr++;
+		}
+	}
+
+	while (leftPtr <= m) {
+		helper.push_back(v[leftPtr]);
+		leftPtr++;
+	}
+
+	while (rightPtr <= r) {
+		helper.push_back(v[rightPtr]);
+		rightPtr++;
+	}
+
+	for (int i = 0; i < helper.size(); i++) {
+		v[l+i] = helper[i];
+	}
 }
 
 void mergesort(vector<int> &v, int l, int r) {
-	int m = (l + r) / 2;
-	mergesort(v, l, m);
-	mergesort(v, m + 1, r);
-	merge(v, l, r);
+	if (l < r) {
+		int m = (l + r) / 2;
+		mergesort(v, l, m); // left
+		mergesort(v, m + 1, r); // right
+		merge(v, l, r);
+	}
 }
 
 int main() {
-	vector<int> testVec{3, 2, 5, 1, 4, 6, 0};
+	vector<int> testVec{3, 2, 5, 1, 4, 6, 0,7};
 
 	cout << "Original array\n";
 	printVec(testVec);
