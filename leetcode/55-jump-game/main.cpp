24a6e13f4139793a5d99d4d12a04caf20a4fd13a
samclee <samclee@gmail.com>
2018-12-06 23:54:24 -0800

    jump game is my bane

--- leetcode/55-jump-game/main.cpp
+++ leetcode/55-jump-game/main.cpp
@@ -3,8 +3,17 @@
 using std::cout;
 using std::vector;
 
+bool canJump(vector<int>& nums) {
+	for (int i = nums.size()-1; i >= 0; i--) {
+		if (nums[i] == 0) {
+			
+		}
+	}       
+}
+
 int main() {
 
+	
 
 	return 0;
 }
