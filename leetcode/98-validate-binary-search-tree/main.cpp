e169c3ee4ca92909cd9c2cf0e832a06c21a3fe3c
samclee <samclee@gmail.com>
2018-12-09 21:35:28 -0600

    godDAMN this is the second time I've overlooked data type nuances

---
+++ leetcode/98-validate-binary-search-tree/main.cpp
@@ -0,0 +1,24 @@
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool helper(TreeNode* root, long min, long max) {
+    if (!root)
+        return true;
+    if (root->val <= min || root->val >= max)
+        return false;
+    
+    return helper(root->left, min, root->val) && helper(root->right, root->val, max);
+}
+
+bool isValidBST(TreeNode* root) {
+    return helper(root, LONG_MIN, LONG_MAX);
+}
+
+int main() {
+	// bleh I don't want to make a test case
+	return 0;
+}
