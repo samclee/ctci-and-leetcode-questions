6298e1ed63cfd99eae13b86bda389f1e5e15dfd5
samclee <samclee@gmail.com>
2019-01-04 11:20:31 -0800

    moar dynamic

--- basic-stuff/dynamic-stuff/knapsack/main.cpp
+++ basic-stuff/dynamic-stuff/knapsack/main.cpp
@@ -2,31 +2,57 @@
 #include <vector>
 using namespace::std;
 
-int subset_sum_helper(int cur, vector<int> wgts, vector<vector<int>>& memo, int target) {
-	// if wgt of curelt > target, definitely can't use it, return memo[curelt-1][target]
+int subset_sum_helper(int cur, vector<int> wgts, vector<vector<int>>& tbl, int target) {
+	// if wgt of curelt > target, definitely can't use it, return tbl[curelt-1][target]
 	if (wgts[cur] > target) {
-		return memo[cur-1][target];
+		return tbl[cur-1][target];
 	}
 	// else, return max of not using it and using it
-	return max(memo[cur-1][target], wgts[cur] + memo[cur-1][target-wgts[cur]]);
+	return max(tbl[cur-1][target], wgts[cur] + tbl[cur-1][target-wgts[cur]]);
 }
 
+// bottom-up
 int subset_sum(vector<int> wgts, int target) {
 	// make wgts.size+1 x target+1 mx
-	vector<vector<int>> memo(wgts.size(), vector<int>(target+1,0));
+	wgts.insert(wgts.begin(),0);
+	vector<vector<int>> tbl(wgts.size(), vector<int>(target+1,0));
 	// run helper on each
 	for (int i=1; i < wgts.size(); i++) {
 		for (int t=0; t <= target; t++) {
-			memo[i][t] = subset_sum_helper(i,wgts,memo,t);
+			tbl[i][t] = subset_sum_helper(i,wgts,tbl,t);
 		}
 	}
 
-	return memo[memo.size()-1][target];
+	return tbl[tbl.size()-1][target];
 }
+
+// top-down
+int lcs(string s1, string s2, int len1, int len2, vector<vector<int>>& memo) {
+	cout << len1 << " " << len2 << endl;
+	if (len1==0 || len2==0)
+		return 0;
+	else if (memo[len1-1][len2-1] != -1)
+		return memo[len1-1][len2-1];
+	else if (s1[len1-1] == s2[len2-1]) {
+		memo[len1-1][len2-1] = 1+lcs(s1,s2,len1-1,len2-1,memo);
+	}
+	else if (s1[len1-1 != s2[len2-1]]) {
+		memo[len1-1][len2-1] = max(lcs(s1,s2,len1,len2-1,memo),lcs(s1,s2,len1-1,len2,memo));
+	}
+
+	return memo[len1-1][len2-1];
+}
+
+int lcs(string s1, string s2) {
+	vector<vector<int>> memo(s1.size(),vector<int>(s2.size(),-1));
+
+	return lcs(s1,s2,s1.size(),s2.size(),memo);
+}
+
 
 int main() {
 	//vector<int> test{1,4,7,2,5,3};
-	vector<int> test{0,2,2,3};
-	cout << subset_sum(test, 6) << endl;
+	vector<int> test{2,2,3};
+	cout << lcs("adbc","acbdcp") << endl;
 	return 0;
 }
